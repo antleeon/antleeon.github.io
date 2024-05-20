1924a31973d246baf51848ead7a845bcf7737fa6
Anton Antleeõn <162236629+antleeon@users.noreply.github.com>
2024-05-20 13:42:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+var slideIndex = 1;
+
 function OpenNthSlide(n) {
   let modal = document.getElementById("slideshow_modal");
+  let img = document.getElementsByClassName("slideshow-image")[n - 1];
   let modalImg = document.getElementsByClassName("modal-image")[0];
   modal.style.display = "block";
-  modalImg.src = this.src;
+  modalImg.src = img.src;
+  slideIndex = n;
+}
+
+function PlusSlides(n) {
+  OpenNthSlide(slideIndex + n);
 }
 
 function CloseSlides() {
