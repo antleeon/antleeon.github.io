4e4e78a4553891759c767ca630f7f443415ec460
Anton Antleeõn <162236629+antleeon@users.noreply.github.com>
2024-05-20 14:08:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 var slideIndex = 1;
+var minIndex = 1;
+var maxIndex = 3;
 
 function OpenNthSlide(n) {
   let modal = document.getElementById("slideshow_modal");
   let img = document.getElementsByClassName("slideshow-image")[n - 1];
   let modalImg = document.getElementsByClassName("modal-image")[0];
+  
   modal.style.display = "block";
   modalImg.src = img.src;
   slideIndex = n;
+  
+  let prev_btn = document.getElementsByClassName("prev-button")[0];
+  let next_btn = document.getElementsByClassName("next-button")[0];
+  
+  if (slideIndex == minIndex) { prev_btn.style.display = "none"; }
+  else { prev_btn.style.display = "flex"; }
+  if (slideIndex == maxIndex) { next_btn.style.display = "none"; }
+  else { next_btn.style.display = "flex"; }
 }
 
 function PlusSlides(n) {
